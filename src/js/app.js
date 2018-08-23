3690024875c01e0e1ea9e69e979eb7d51207b0ed
VolodymyrMarchenko <nokialeader@gmail.com>
2018-08-23 15:16:22 +0300

    grid-flex

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,15 @@
 import sayHello from './lib/sayHello.js';
 
 sayHello();
+
+$('.nav-main li').click(function() {
+  $(this).find('.submenu').slideToggle('fast');
+});
+$(document).on('click', function(event) {
+  var trigger = $('.nav-main li');
+  if(trigger !== event.target && !trigger.has(event.target).length) {
+    $('.submenu').slideUp('fast');
+  } else {
+    return false;
+  }  
+});
