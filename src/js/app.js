b592d47f47b80bd6c095550faed96a7f4c0137fd
VolodymyrMarchenko <nokialeader@gmail.com>
2018-08-23 15:37:12 +0300

    remove drop-box

--- src/js/app.js
+++ src/js/app.js
@@ -2,14 +2,14 @@ import sayHello from './lib/sayHello.js';
 
 sayHello();
 
-$('.nav-main li').click(function() {
-  $(this).find('.submenu').slideToggle('fast');
-});
-$(document).on('click', function(event) {
-  var trigger = $('.nav-main li');
-  if(trigger !== event.target && !trigger.has(event.target).length) {
-    $('.submenu').slideUp('fast');
-  } else {
-    return false;
-  }  
-});
+// $('.nav-main li').click(function() {
+//   $(this).find('.submenu').slideToggle('fast');
+// });
+// $(document).on('click', function(event) {
+//   var trigger = $('.nav-main li');
+//   if(trigger !== event.target && !trigger.has(event.target).length) {
+//     $('.submenu').slideUp('fast');
+//   } else {
+//     return false;
+//   }  
+// });
