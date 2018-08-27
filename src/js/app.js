324c5dd69d02b13f55587bcf5bed8513492d374f
VolodymyrMarchenko <nokialeader@gmail.com>
2018-08-27 18:50:29 +0300

    some add

--- src/js/app.js
+++ src/js/app.js
@@ -2,14 +2,23 @@ import sayHello from './lib/sayHello.js';
 
 sayHello();
 
-// $('.nav-main li').click(function() {
-//   $(this).find('.submenu').slideToggle('fast');
-// });
-// $(document).on('click', function(event) {
-//   var trigger = $('.nav-main li');
-//   if(trigger !== event.target && !trigger.has(event.target).length) {
-//     $('.submenu').slideUp('fast');
-//   } else {
-//     return false;
-//   }  
-// });
+                                                   
+$(document).on('click', function(event) {
+  var trigger = $('.dropdown');
+  if(trigger !== event.target && !trigger.has(event.target).length) {
+    $('.submenu').slideUp('fast');
+  } 
+  else if(trigger !== !event.target) {
+    $('.submenu').toggleClass('submenu.is-active');
+  };           
+});
+
+$('.dropdown').click(function() {
+  $(this).find('.submenu').slideToggle('fast');
+}); 
+
+
+$('.js-burger').on('click', function() {
+  var targetBtn = $(this).find('.btn-nav').data('target');
+  $(targetBtn).toggleClass('nav__list_is-open');
+});
